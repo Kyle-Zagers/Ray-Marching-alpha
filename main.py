46f3a3c9a2dd175dad978b8e8fdc35e3d559f3a6
Kyle-Zagers <kylezagers@ufl.edu>
2023-11-18 17:48:14 -0500

    Adding boiler plate code for ModernGL along with custom test shaders.

---
+++ main.py
@@ -0,0 +1,124 @@
+from pyrr import Vector3, vector, vector3, matrix44
+import moderngl_window as mglw
+
+from camera import Camera
+
+class App(mglw.WindowConfig):
+    WIDTH, HEIGHT = 1920, 1080
+    window_size = WIDTH, HEIGHT
+    resource_dir = 'programs'
+    cam = Camera();
+    lastX, lastY = WIDTH/2, HEIGHT/2
+    first_mouse = True
+    cam_pos = Vector3([0.0, 2.0, -4.0])
+    keys = {}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.quad = mglw.geometry.quad_fs()
+        self.program = self.load_program(vertex_shader='vertex.glsl', fragment_shader='fragment.glsl')
+        self.u_scroll = 3.0
+        # keys
+
+        # textures
+        self.texture1 = self.load_texture_2d('../textures/test0.png')
+        self.texture2 = self.load_texture_2d('../textures/hex.png')  # floor
+        self.texture3 = self.load_texture_2d('../textures/white_marble1.png')  # walls
+        self.texture4 = self.load_texture_2d('../textures/roof/texture3.jpg')  # roof
+        self.texture5 = self.load_texture_2d('../textures/black_marble1.png')  # pedestal
+        self.texture6 = self.load_texture_2d('../textures/green_marble1.png')  # sphere
+        self.texture7 = self.load_texture_2d('../textures/roof/height3.png')  # roof bump
+        # uniforms
+        self.program['u_scroll'] = self.u_scroll
+        self.program['u_resolution'] = self.window_size
+        self.program['u_camPos'] = self.cam_pos
+        # self.program['u_texture1'] = 1
+        # self.program['u_texture2'] = 2
+        # self.program['u_texture3'] = 3
+        # self.program['u_texture4'] = 4
+        # self.program['u_texture5'] = 5
+        # self.program['u_texture6'] = 6
+        # self.program['u_texture7'] = 7
+
+    
+        
+
+    def render(self, time, frame_time):
+        self.ctx.clear()
+        self.program['u_time'] = time
+        self.key_press()
+        # self.texture1.use(location=1)
+        # self.texture2.use(location=2)
+        # self.texture3.use(location=3)
+        # self.texture4.use(location=4)
+        # self.texture5.use(location=5)
+        # self.texture6.use(location=6)
+        # self.texture7.use(location=7)
+        self.quad.render(self.program)
+
+    def mouse_position_event(self, x, y, dx, dy):
+        #self.program['u_mouse'] = (x, y)
+        pass
+    
+    def mouse_scroll_event(self, x_offset, y_offset):
+        self.u_scroll = max(1.0, self.u_scroll + y_offset)
+        self.program['u_scroll'] = self.u_scroll
+    
+
+    def key_event(self, key, action, modifiers):
+        if key == self.wnd.keys.W:
+            self.keys["W"] = action == self.wnd.keys.ACTION_PRESS
+        if key == self.wnd.keys.A:
+            self.keys["A"] = action == self.wnd.keys.ACTION_PRESS
+        if key == self.wnd.keys.S:
+            self.keys["S"] = action == self.wnd.keys.ACTION_PRESS
+        if key == self.wnd.keys.D:
+            self.keys["D"] = action == self.wnd.keys.ACTION_PRESS
+        if key == self.wnd.keys.SPACE:
+            self.keys["SPACE"] = action == self.wnd.keys.ACTION_PRESS
+        #if modifiers == self.wnd.modifiers.ctrl:
+        self.keys["shift"] = self.wnd.modifiers.shift;
+
+        self.program['u_camPos'] = self.cam_pos
+        
+
+    def key_press(self):
+        speed = 0.05
+        if self.keys.get("W"):
+            self.cam_pos += Vector3([0.0, 0.0, speed])
+        if self.keys.get("A"):
+            self.cam_pos += Vector3([-speed, 0.0, 0.0])
+        if self.keys.get("S"):
+            self.cam_pos += Vector3([0.0, 0.0, -speed])
+        if self.keys.get("D"):
+            self.cam_pos += Vector3([speed, 0.0, 0.0])
+        if self.keys.get("SPACE"):
+            self.cam_pos += Vector3([0.0, speed, 0.0])
+        if self.keys.get("shift"):
+            self.cam_pos += Vector3([0.0, -speed, 0.0])
+        self.program['u_camPos'] = self.cam_pos
+
+    # def assign_event_callbacks(self):
+    #     return super().assign_event_callbacks()
+
+    # def mouse_look_clb(self, window, xpos, ypos):
+    #     if self.first_mouse:
+    #         self.lastX = xpos
+    #         self.lastY = ypos
+
+    #     xOffset = xpos - self.lastX
+    #     yOffset = self.lastY - ypos
+
+    #     self.lastX = xpos
+    #     self.lastY = ypos
+
+    #     self.cam.process_mouse_movement(xOffset, yOffset)
+
+    # def mouse_enter_clb(self, window, entered):
+    #     self.first_mouse = not entered
+
+            
+
+
+if __name__ == '__main__':
+    mglw.run_window_config(App)
---
+++ programs/kaifrag.glsl
@@ -0,0 +1,162 @@
+#version 330 core
+layout (location = 0) out vec4 fragColor;
+
+// Helful Resources
+// ----------------
+// Ray Marching Blog Post by Michael Walczyk
+// https://michaelwalczyk.com/blog-ray-marching.html
+// Inigo Quilez SDF Functions
+// https://iquilezles.org/articles/distfunctions/
+
+precision mediump float;
+
+uniform float u_time;
+uniform vec2 u_resolution;
+uniform vec3 position;
+uniform  vec2 u_mouse;
+
+const float NUM_OF_STEPS = 128.0;
+const float MIN_DIST_TO_SDF = 0.001;
+const float MAX_DIST_TO_TRAVEL = 64.0;
+
+
+
+float opSmoothUnion(float d1, float d2, float k) {
+  float h = clamp(0.5 + 0.5 * (d2 - d1) / k, 0.0, 1.0);
+  return mix(d2, d1, h) - k * h * (1.0 - h);
+}
+
+float sdfPlane(vec3 p, vec3 n, float h) {
+  // n must be normalized
+  return dot(p, n) + h;
+}
+
+float sdfSphere(vec3 p, vec3 c, float r) {
+  vec3 moddy = p;
+  moddy.xz = mod(moddy.xz, 1.0)-0.5;
+  vec3 movvy = c;
+  movvy.y = movvy.y + sin(length(p-c)*movvy.y*0.1);
+  return length(moddy-movvy) - r/2.0;
+}
+
+float map(vec3 p) {
+  float radius = 0.75;
+  vec3 center = vec3(0.0);
+
+  // part 4 - change height of the sphere based on time
+  center = vec3(0.0, -0.25 + sin(u_time) * 0.5-1.0, 0.0);
+
+  float sphere = sdfSphere(p, center, radius);
+  float m = sphere;
+
+
+  // part 1.2 - display plane
+  float h = 1.0;
+  vec3 normal = vec3(0.0, 1.0, 0.0);
+  float plane = sdfPlane(p, normal, h);
+  m = min(sphere, plane);
+
+  // part 4 - add smooth blending
+  m = opSmoothUnion(sphere, plane, 0.5);
+
+  return m;
+}
+
+float rayMarch(vec3 ro, vec3 rd, float maxDistToTravel) {
+  float dist = 0.0;
+
+  for (float i = 0.0; i < NUM_OF_STEPS; i++) {
+    vec3 currentPos = ro + rd * dist;
+    float distToSdf = map(currentPos);
+
+    if (distToSdf < MIN_DIST_TO_SDF) {
+      break;
+    }
+
+    dist = dist + distToSdf;
+
+    if (dist > maxDistToTravel) {
+      break;
+    }
+  }
+
+  return dist;
+}
+
+vec3 getNormal(vec3 p) {
+  vec2 d = vec2(0.01, 0.0);
+  float gx = map(p + d.xyy) - map(p - d.xyy);
+  float gy = map(p + d.yxy) - map(p - d.yxy);
+  float gz = map(p + d.yyx) - map(p - d.yyx);
+  vec3 normal = vec3(gx, gy, gz);
+  return normalize(normal);
+}
+
+vec3 render(vec2 uv) {
+  vec3 color = vec3(0.0);
+
+  // note: ro -> ray origin, rd -> ray direction
+  vec3 ro = position;
+  vec3 rd = vec3(uv, 1.0);
+
+  float dist = rayMarch(ro, rd, MAX_DIST_TO_TRAVEL);
+
+  if (dist < MAX_DIST_TO_TRAVEL) {
+    // part 1 - display ray marching result
+    color = vec3(1.0);
+
+    // part 2.1 - calculate normals
+    // calculate normals at the exact point where we hit SDF
+    vec3 p = ro + rd * dist;
+    vec3 normal = getNormal(p);
+    color = normal;
+
+    // part 2.2 - add lighting
+
+    // part 2.2.1 - calculate diffuse lighting
+    vec3 lightColor = vec3(1.0);
+    vec3 lightSource = vec3(2.5, 2.5, -1.0);
+    float diffuseStrength = max(0.0, dot(normalize(lightSource), normal));
+    vec3 diffuse = lightColor * diffuseStrength;
+
+    // part 2.2.2 - calculate specular lighting
+    vec3 viewSource = normalize(ro);
+    vec3 reflectSource = normalize(reflect(-lightSource, normal));
+    float specularStrength = max(0.0, dot(viewSource, reflectSource));
+    specularStrength = pow(specularStrength, 64.0);
+    vec3 specular = specularStrength * lightColor;
+
+    // part 2.2.3 - calculate lighting
+    vec3 lighting = diffuse * 0.75 + specular * 0.25;
+    color = lighting;
+
+    // part 3 - add shadows
+
+    // part 3.1 - update the ray origin and ray direction
+    vec3 lightDirection = normalize(lightSource);
+    float distToLightSource = length(lightSource - p);
+    ro = p + normal * 0.1;
+    rd = lightDirection;
+
+    // part 3.2 - ray march based on new ro + rd
+    float dist = rayMarch(ro, rd, distToLightSource);
+    if (dist < distToLightSource) {
+      color = color * vec3(0.25);
+    }
+
+    // note: add gamma correction
+    color = pow(color, vec3(1.0 / 2.2));
+  }
+
+  return color;
+}
+
+void main() {
+  vec2 uv = 2.0 * gl_FragCoord.xy / u_resolution - 1.0;
+  // note: properly center the shader in full screen mode
+  uv = (2.0 * gl_FragCoord.xy - u_resolution) / u_resolution.y;
+  vec3 color = vec3(0.0);
+  color = render(uv);
+  //color = vec3(uv, 0.0);
+  fragColor = vec4(color, 1.0);
+}
